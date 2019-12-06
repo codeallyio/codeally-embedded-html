9a4094e59d73bc673fca1976cff26fa260a45b74
adamzaczek <adamzaczekul@gmail.com>
2019-12-06 17:44:11 +0100

    Update main example

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 window.onload = () => {
-  const htmlEditor = CodeMirror.fromTextArea(document.getElementById("code"), {
+  const editor = CodeMirror.fromTextArea(document.getElementById("code"), {
     lineNumbers: true,
-    theme: "dracula"
+    theme: "dracula",
+    readOnly: true
   });
+
+  editor.getDoc().setValue(`
+<iframe
+  title="Strove.io"
+  wmode="transparent"
+  style="width: 100%; height: 550px; position: relative; overflow: hidden; border: 0;"
+  src="https://strove.io/embed/?repoUrl=https://github.com/stroveio/strove.io"
+  sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin"
+></iframe>
+`);
 };
