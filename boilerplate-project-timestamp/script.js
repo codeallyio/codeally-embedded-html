7958e40e92de17e2f38106671a55f93fd982cb3b
jaroszpiotr91 <jarosz.piotr91@gmail.com>
2019-12-18 17:00:45 +0100

    Fix

--- boilerplate-project-timestamp/script.js
+++ boilerplate-project-timestamp/script.js
@@ -13,7 +13,7 @@ window.onload = () => {
     title="Strove.io"
     wmode="transparent"
     style="width: 100%; height: 550px; position: relative; overflow: hidden; border: 0;"
-    src="https://github.com/stroveio/boilerplate-project-timestamp.git"
+    src="https://strove.io/embed/?repoUrl=https://github.com/stroveio/boilerplate-project-timestamp&goBackToRoute=/boilerplate-project-timestamp"
     sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin"
   ></iframe>
   `);
