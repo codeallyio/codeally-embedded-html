bad1ce06254a1dab41d5c6b70ded69b8db505fb9
adamzaczek <adamzaczekul@gmail.com>
2019-12-18 21:41:36 +0100

    Update examples

--- boilerplate-project-metricimpconverter/script.js
+++ boilerplate-project-metricimpconverter/script.js
@@ -1,21 +1,20 @@
 window.onload = () => {
-    const fullstackEditor = CodeMirror.fromTextArea(
-      document.getElementById("fullstackCode"),
-      {
-        lineNumbers: true,
-        theme: "dracula",
-        readOnly: true
-      }
-    );
-  
-    fullstackEditor.getDoc().setValue(`
+  const fullstackEditor = CodeMirror.fromTextArea(
+    document.getElementById("fullstackCode"),
+    {
+      lineNumbers: true,
+      theme: "dracula",
+      readOnly: true
+    }
+  );
+
+  fullstackEditor.getDoc().setValue(`
     <iframe
       title="Strove.io"
       wmode="transparent"
       style="width: 100%; height: 550px; position: relative; overflow: hidden; border: 0;"
-      src="https://strove.io/embed/?repoUrl=https://github.com/stroveio/boilerplate-project-metricimpconverter&goBackToRoute=/boilerplate-project-metricimpconverter"
+      src="https://strove.io/embed/?repoUrl=https://github.com/stroveio/boilerplate-project-metricimpconverter&goBackTo=https://embed.strove.io/boilerplate-project-metricimpconverter"
       sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin"
     ></iframe>
     `);
-  };
-  
+};
--- hackathon-starter-strove/script.js
+++ hackathon-starter-strove/script.js
@@ -13,7 +13,7 @@ window.onload = () => {
     title="Strove.io"
     wmode="transparent"
     style="width: 100%; height: 550px; position: relative; overflow: hidden; border: 0;"
-    src="https://strove.io/embed/?repoUrl=https://github.com/stroveio/hackathon-starter-strove&goBackToRoute=/hackathon-starter-strove"
+    src="https://strove.io/embed/?repoUrl=https://github.com/stroveio/hackathon-starter-strove&goBackTo=https://embed.strove.io/hackathon-starter-strove"
     sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin"
   ></iframe>
   `);
