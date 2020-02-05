7d46706722f7434d13b4f774bbaacb70718337b0
jaroszpiotr91 <jarosz.piotr91@gmail.com>
2020-02-05 15:43:22 +0100

    Added stripe example for node

---
+++ stripe-node/script.js
@@ -0,0 +1,21 @@
+window.onload = () => {
+    const fullstackEditor = CodeMirror.fromTextArea(
+      document.getElementById("fullstackCode"),
+      {
+        lineNumbers: true,
+        theme: "dracula",
+        readOnly: true
+      }
+    );
+  
+    fullstackEditor.getDoc().setValue(`
+      <iframe
+        title="Strove.io"
+        wmode="transparent"
+        style="width: 100%; height: 550px; position: relative; overflow: hidden; border: 0;"
+        src="https://strove.io/embed/?repoUrl=https://github.com/stroveio/stripe-example.git&goBackTo=https://embed.strove.io/stripe-node"
+        sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin"
+      ></iframe>
+      `);
+  };
+  
