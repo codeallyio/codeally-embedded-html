dbe8c34f7719d46ea8d972004967a42917ceca4c
adamzaczek <adamzaczekul@gmail.com>
2021-05-15 23:31:48 +0200

    New example

--- learn-git-by-building-an-sql-reference-object/script.js
+++ learn-git-by-building-an-sql-reference-object/script.js
@@ -13,7 +13,7 @@ window.onload = () => {
     title="codeally.io"
     wmode="transparent"
     style="width: 100%; height: 550px; position: relative; overflow: hidden; border: 0;"
-    src="https://codeally.io/embed/?repoUrl=https://codeally.io/embed/?repoUrl=https://github.com/moT01/.learn-bash-scripting-by-building-five-programs.git&goBackTo=https://github.com/moT01/.learn-bash-scripting-by-building-five-programs.git"
+    src="https://codeally.io/embed/?repoUrl=https://codeally.io/embed/?repoUrl=https://github.com/moT01/.learn-git-by-building-an-sql-reference-object"
     sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin"
   ></iframe>
   `);
