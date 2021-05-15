341b7de0e702683511d2711d1da8f029ae837d74
adamzaczek <adamzaczekul@gmail.com>
2021-05-15 23:17:03 +0200

    New example

---
+++ learn-bash-scripting-by-building-five-programs/script.js
@@ -0,0 +1,20 @@
+window.onload = () => {
+  const fullstackEditor = CodeMirror.fromTextArea(
+    document.getElementById("fullstackCode"),
+    {
+      lineNumbers: true,
+      theme: "dracula",
+      readOnly: true,
+    }
+  );
+
+  fullstackEditor.getDoc().setValue(`
+  <iframe
+    title="codeally.io"
+    wmode="transparent"
+    style="width: 100%; height: 550px; position: relative; overflow: hidden; border: 0;"
+    src="https://codeally.io/embed/?repoUrl=https://github.com/stroveio/hackathon-starter-strove&goBackTo=https://embed.codeally.io/hackathon-starter-strove"
+    sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin"
+  ></iframe>
+  `);
+};
