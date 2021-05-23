51f07ea68f6ce69e04a5068da39f531a0130b6e2
adamzaczek <adamzaczekul@gmail.com>
2021-05-23 23:50:01 +0200

    Update example urls

--- learn-bash-scripting-by-building-five-programs/script.js
+++ learn-bash-scripting-by-building-five-programs/script.js
@@ -13,7 +13,7 @@ window.onload = () => {
     title="codeally.io"
     wmode="transparent"
     style="height: 550px; position: relative; overflow: hidden; border-top: 2px solid black; border-bottom: 2px solid black; width: 100vw; left: -40px;"
-    src="https://codeally.io/embed/?repoUrl=https://codeally.io/embed/?repoUrl=https://github.com/moT01/.learn-bash-scripting-by-building-five-programs.git&goBackTo=https://github.com/moT01/.learn-bash-scripting-by-building-five-programs.git"
+    src="https://codeally.io/embed/?repoUrl=https://github.com/moT01/.learn-bash-scripting-by-building-five-programs.git&goBackTo=https://github.com/moT01/.learn-bash-scripting-by-building-five-programs.git"
     sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin"
   ></iframe>
   `);
