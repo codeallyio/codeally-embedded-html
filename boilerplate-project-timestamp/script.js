bef08b49682eb6267ecc6843bc3f54192b649eb8
adamzaczek <adamzaczekul@gmail.com>
2021-05-23 23:59:14 +0200

    Update example urls

--- boilerplate-project-timestamp/script.js
+++ boilerplate-project-timestamp/script.js
@@ -13,7 +13,7 @@ window.onload = () => {
     title="codeally.io"
     wmode="transparent"
     style="height: 550px; position: relative; overflow: hidden; border-top: 2px solid black; border-bottom: 2px solid black; width: 100vw; left: -40px;"
-    src="https://codeally.io/embed/?repoUrl=https://github.com/codeallyio/boilerplate-project-timestamp&goBackTo=https://embed.codeally.io/boilerplate-project-timestamp"
+    src="https://codeally.io/embed/?repoUrl=https://github.com/codeallyio/boilerplate-project-timestamp"
     sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin"
   ></iframe>
   `);
--- hackathon-starter-codeally/script.js
+++ hackathon-starter-codeally/script.js
@@ -13,7 +13,7 @@ window.onload = () => {
     title="codeally.io"
     wmode="transparent"
     style="height: 550px; position: relative; overflow: hidden; border-top: 2px solid black; border-bottom: 2px solid black; width: 100vw; left: -40px;"
-    src="https://codeally.io/embed/?repoUrl=https://github.com/codeallyio/hackathon-starter-codeally&goBackTo=https://embed.codeally.io/hackathon-starter-codeally"
+    src="https://codeally.io/embed/?repoUrl=https://github.com/codeallyio/hackathon-starter-codeally"
     sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin"
   ></iframe>
   `);
